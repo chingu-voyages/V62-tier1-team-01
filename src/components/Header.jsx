d28f48a992b6f95bfcbb5c4984b7df8bfcf6a43d
Claire Lee <llsy9716@gmail.com>
2026-09-17 13:22:32 -0600

    feat: add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+// todo
+
+import searchIcon from "../assets/search-icon.svg";
+import "./Header.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>NEXA</h1>
+
+      <nav className="navigation">
+        <a href="/">Home</a>
+        <a href="/">My Learning</a>
+
+        <button className="btn">
+          <img src="Design/search-icon.svg" alt="Search" />
+        </button>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
