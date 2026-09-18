068eb24209733be1e4c0a21a5410af57dee06c21
umfrancisco <francis.guitler@gmail.com>
2026-09-18 10:14:36 -0300

    feat: add css modules to avoid global css conflicts

--- src/components/LandingPage/index.jsx
+++ src/components/LandingPage/index.jsx
@@ -1,35 +1,36 @@
 import bannerImg from "/src/assets/banner-img.svg";
 import playButton from "/src/assets/play-button.svg";
+import styles from "./LandingPage.module.css";
 
 function LandingPage({ onNextStep }) {
   return (
     <div className="container">
-      <div className="lp-box">
-        <div className="lp-text-section">
+      <div className={styles.lpBox}>
+        <div className={styles.lpTextSection}>
           <div>
-            <span className="lp-title-small">AI-POWERED LEARNING PATHS</span>
-            <h2 className="lp-title">
+            <span className={styles.lpTitleSmall}>AI-POWERED LEARNING PATHS</span>
+            <h2 className={styles.lpTitle}>
               From Curiosity
               <br />
               to Career.
             </h2>
-            <p className="lp-text">
+            <p className={styles.lpText}>
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
               aliquid rerum dolor vitae voluptas minima harum, in quam corporis
               consectetur debitis molestias recusandae, explicabo distinctio.
             </p>
           </div>
-          <div className="buttons-box">
-            <button className="button-next-page" onClick={onNextStep}>
+          <div className={styles.buttonsBox}>
+            <button className={styles.buttonNextPage} onClick={onNextStep}>
               <span>Generate your path</span>
             </button>
-            <button className="button-play">
-              <img className="icon-play" src={playButton} alt="play button" />
+            <button className={styles.buttonPlay}>
+              <img className={styles.iconPlay} src={playButton} alt="play button" />
               <span>Watch demo</span>
             </button>
           </div>
         </div>
-        <img className="banner-img" src={bannerImg} alt="banner image" />
+        <img className={styles.bannerImg} src={bannerImg} alt="banner image" />
       </div>
     </div>
   );
