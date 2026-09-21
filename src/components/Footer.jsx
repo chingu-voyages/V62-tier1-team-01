7b7663dbec16bb05a8f61374c7a6046e79ee1a9c
Claire Lee <llsy9716@gmail.com>
2026-09-21 15:57:47 -0600

    feat: added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,67 @@
-// todo
+import { FaGithub, FaLinkedin, FaYoutube, FaXTwitter } from "react-icons/fa6";
+import brandIcon from "../assets/logo.png";
+import "./Footer.css";
+
 function Footer() {
-  return <div>Footer</div>;
-}
+  return (
+      <footer className="footer">
+        <div className="brand-logo">
+          <img src={brandIcon} alt="Brand Icon" className="brand-icon" />
+        </div>
+  
+        <nav className="footer-menu">
+
+          <div className="footer-menu-column">
+            <h3>Product</h3>
+            <a href="#">Features</a>
+            <a href="#">Pricing</a>
+            <a href="#">Changelog</a>
+          </div>
+
+          <div className="footer-menu-column">
+            <h3>Resources</h3>
+            <a href="#">Blog</a>
+            <a href="#">Learning Guides</a>
+            <a href="#">Help Center</a>
+          </div>
+
+          <div className="footer-menu-column">
+            <h3>Community</h3>
+            <a href="#">Discord</a>
+            <a href="#">Forum</a>
+            <a href="#">Events</a>
+          </div>
+
+          <div className="footer-menu-column">
+            <h3>Company</h3>
+            <a href="#">About</a>
+            <a href="#">Careers</a>
+            <a href="#">Contact</a>
+          </div>
+
+        </nav>
+
+        <div className="footer-social-media">
+          <div className="social-icons">
+            <a href="#" aria-label="GitHub">
+              <FaGithub />
+            </a>
+
+            <a href="#" aria-label="LinkedIn">
+              <FaLinkedin />
+            </a>
+
+            <a href="#" aria-label="YouTube">
+              <FaYoutube />
+            </a>
+
+            <a href="#" aria-label="X">
+              <FaXTwitter />
+            </a>
+          </div>
+        </div>
+      </footer>
+    );
+  }
 
 export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,24 @@
-// todo
+import brandIcon from "../assets/logo.png";
+import searchIcon from "../assets/search-icon.svg";
+import "./Header.css";
+
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <div className="brand-logo">
+        <img src={brandIcon} alt="Brand Icon" className="brand-icon" />
+      </div>
+
+      <nav className="navigation">
+        <a href="/">Home</a>
+        <a href="/">My Learning</a>
+
+        <button className="btn">
+          <img src={searchIcon} alt="Search" />
+        </button>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
